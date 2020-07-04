df50633b9cd3061f9fd942255148748ca2e3e003
Kieran815 <kieran.milligan@gmail.com>
2020-07-04 14:50:40 -0500

    finished main scaffolding, now breaking off to new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import `NgModel` via `@angular/forms`
+import { FormsModule } from '@angular/forms';
+//import `HeroesComponent`
+import { HeroesComponent } from './heroes/heroes.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
